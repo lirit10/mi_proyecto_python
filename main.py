7310e1a786c9e3712449973caf6a44876e148a4e
alum_cees_e <milearre574@gmail.com>
2026-05-05 09:04:11 -0500

    agregada_funcionalidad_para_calcular_area_de_circulos

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-try:
+import math
+print("--- Calculadora de Áreas ---")
+opcion = input("¿Qué quieres calcular? (1: Rectángulo, 2: Círculo): ")
+
+if opcion == "1":
     b = float(input("Ingresa la base: "))
     h = float(input("Ingresa la altura: "))
-    area = (b * h)
-    print(f"El área del rectángulo es: {area}")
-except ValueError:
-    print("Por favor, ingresa solo números válidos.")
+    area_rectangulo = (b * h)
+    print(f"El área es: {area_rectangulo}")
+elif opcion == "2":
+    r = float(input("Ingresa el radio: "))
+    area_circulo = math.pi * (r ** 2)
+    print(f"El área es: {area_circulo}")
+else:
+    print("Opción no válida")
